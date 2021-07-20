0df4572e925583d6150148f7cc89595db14619bb
Kartik Yadav <kartikyadav94@gmail.com>
2021-07-20 22:33:27 +0530

    initial commit

---
+++ web/src/Routes/Routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+export default function Routes {
+    return (
+        <div>
+            <Switch>
+                <Route>
+                    
+                </Route>
+            </Switch>
+        </div>
+    )
+}
