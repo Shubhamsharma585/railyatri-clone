ebded6c9b37dd397c0664c9631373fab3a8fa4fa
Raushan8271 <raushan8271@gmail.com>
2021-07-22 11:10:21 +0530

    navbar

--- web/src/Routes/Routes.tsx
+++ web/src/Routes/Routes.tsx
@@ -1,22 +1,7 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Route, Switch } from 'react-router-dom'
-import Buses from "../Components/Buses/Buses"
-
-
-
-export default function Routes() 
-{
-    
-    return (
-        <div>
-            <Switch>
-                <Route path="/" exact>
-                 <Buses/>
-               </Route>
-=======
 import { Route, Switch, Redirect } from 'react-router-dom'
 import LandingPage from '../Components/landingPage/LandingPage'
+import Buses from "../Components/Buses/Buses"
 
 export default function Routes() {
     return (
@@ -27,8 +12,8 @@ export default function Routes() {
                 </Route>
                 <Route path="/bus-booking">
                     <LandingPage />
+                    <Buses />
                 </Route>
->>>>>>> f7b077d2dcd77d7feb02d5ea86777453478c59a0
             </Switch>
         </div>
     )
