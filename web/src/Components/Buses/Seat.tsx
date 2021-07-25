dbec685e45bc982dabd4ecd413417af7c235de08
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-07-25 11:39:43 +0530

    buses

---
+++ web/src/Components/Buses/Seat.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Styles from "./Seat.module.css"
+import seat from "../../Images/seat.png"
+
+interface ISeat{
+    numb: number 
+}
+
+function Seat({ numb } :ISeat ) {
+    return (
+        <div className={Styles.main}>
+            <p>{numb}</p>
+        </div>
+    )
+}
+
+export default Seat
---
+++ web/src/Redux/Buses/busreducer.ts
@@ -0,0 +1,57 @@
+
+import { BUS_REQUEST, BUS_SUCCESS, BUS_FAILURE } from "./actiontype"
+
+
+
+ 
+const initstate = {
+   
+}
+
+interface Ibuspayload {
+    
+}
+
+
+interface Ibusreducer {
+    type: string,
+    payload: Ibuspayload
+}
+
+
+function busreducer(state = initstate, {type, payload}: Ibusreducer) 
+{
+    console.log(state, type, payload)
+    switch(type)
+    {  
+        case BUS_REQUEST: 
+            { 
+                return {
+                   ...state,
+                   isloading: true,
+                }
+            }
+
+        case BUS_SUCCESS: 
+        {
+            return {
+                ...state,
+                isloading: false,
+            }
+        }
+        case BUS_FAILURE:
+            {
+                return {
+                    ...state,
+                    isloading: false,
+                }
+            }
+
+        default:
+            return { 
+               ...state
+             }   
+    }
+}
+
+export default busreducer;
